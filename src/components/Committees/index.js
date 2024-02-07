51f31f16d7ba867f0a1244ecfa9079144583aa44
soumesh113 <soumesh.khare@gmail.com>
2024-02-07 20:07:33 +0530

    changed committees

--- src/components/Committees/index.js
+++ src/components/Committees/index.js
@@ -13,24 +13,45 @@ export default function Committees() {
         </Heading>
       </Center>
 
-      <SimpleGrid spacing={4} justifyItems='center' minChildWidth='250px'>
+      <SimpleGrid spacing={4} justifyItems='center' minChildWidth='200px'>
         <CommitteesCard
           name='UN General Assembly (UNGA)'
           image='images/unga.jpeg'
           description='The UNGA is the main policy-making organ of the UN. Comprising all Member States, it provides a unique forum for multilateral discussion of the full spectrum of international issues covered by the Charter of the United Nations.'
-          agenda='Addressing the impact of the widespread refugee crisis on social and economic stability'
+          agenda='Navigating the geopolitical gastronomy: With
+          special emphasis on diplimatic formulas to mitigate
+          the confluence of climate crisis, conflict
+          dynamics, and economic uncertainties in the global
+          food nexus'
         />
         <CommitteesCard
           name='UN Security Council (UNSC)'
           image='images/unsa.jpeg'
           description='The Security Council has primary responsibility for the maintenance of international peace and security. It takes the lead in determining the existence of a threat to the peace or act of aggression.'
-          agenda='Disputes and controversies in the South China Sea region'
+          agenda='Enhancing cross-national security cooperation
+          to counter pirate militia attacks in the red sea
+          and safeguarding global shipping routes with a
+          focus on economic stability'
         />
         <CommitteesCard
           name='UN Human Rights Council (UNHRC)'
           image='images/unhrc.jpeg'
           description='UNHRC is a United Nations body whose mission is to promote and protect human rights around the world. It is involved in being more responsive to the needs of victims of rights violations worldwide.'
-          agenda='Ensuring public access to information and protection of fundamental freedom in accordance with national legislation and international agreements'
+          agenda='Harmonizing human rights safeguards in the Israel Palestine conflict: prioritizing the safety of civilians,
+          ensuring accountability for violations, facilitating
+          adequate humanitarian aid, and cultivating
+          international cooperation for a just and peaceful
+          resolution'
+        />
+                <CommitteesCard
+          name='UN General Assembly (UNGA)'
+          image='images/unga.jpeg'
+          description='The UNGA is the main policy-making organ of the UN. Comprising all Member States, it provides a unique forum for multilateral discussion of the full spectrum of international issues covered by the Charter of the United Nations.'
+          agenda='Navigating the geopolitical gastronomy: With
+          special emphasis on diplimatic formulas to mitigate
+          the confluence of climate crisis, conflict
+          dynamics, and economic uncertainties in the global
+          food nexus'
         />
       </SimpleGrid>
 
